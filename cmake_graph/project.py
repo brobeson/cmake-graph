120f14df74cbc65a97c16467438a21e533605683
brobeson <brobeson@users.noreply.github.com>
2019-10-10 14:43:01 -0600

    add a Project class
    The class will encapsulate a CMake project and its dependencies. I also
    added a unit test.

---
+++ cmake_graph/project.py
@@ -0,0 +1,11 @@
+"""Provides the Project class."""
+
+class Project:
+    """Encapsulates a project and its dependencies."""
+
+    def __init__(self, name: str):
+        pass
+
+    @property
+    def name(self):
+        """Get the name of this project."""
