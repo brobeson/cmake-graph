72b13ab287c5256da1a6140156bf41c1e950c081
brobeson <brobeson@users.noreply.github.com>
2019-10-10 16:51:20 -0600

    add target type to the Target class
    I updated the Target unit test for the new functionality.

--- cmake_graph/target.py
+++ cmake_graph/target.py
@@ -1,12 +1,27 @@
 """Provides the Target class."""
 
 
+LIBRARY_TARGET = "library"
+EXECUTABLE_TARGET = "executable"
+
 class Target:
     """Encapsulates a target and its dependencies."""
 
-    def __init__(self, name: str):
-        pass
+    def __init__(self, name: str, target_type: str):
+        """Construct a Target object.
+
+        :param str name: The target's name. This should not be the alias name
+            for a target created within the project, but the actual target name.
+        :param str target_type: The type of target. See :ref:TARGET_TYPES for
+            acceptable values.
+        :raises ValueError: if any of the positional arguments are ``None`` or
+            empty strings.
+        """
 
     @property
     def name(self):
         """Get the name of this target."""
+
+    @property
+    def target_type(self):
+        """Get the type of this target."""
--- unit_tests/test_target.py
+++ unit_tests/test_target.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from cmake_graph.target import Target
+from cmake_graph.target import Target, LIBRARY_TARGET
 
 
 class TargetTest(unittest.TestCase):
@@ -14,14 +14,23 @@ class TargetTest(unittest.TestCase):
         """
         # pylint: disable=unused-variable
         with self.assertRaises(ValueError):
-            p = Target(None)
+            p = Target(None, LIBRARY_TARGET)
         with self.assertRaises(ValueError):
-            p = Target("")
+            p = Target("", LIBRARY_TARGET)
+        with self.assertRaises(ValueError):
+            p = Target("foo", None)
+        with self.assertRaises(ValueError):
+            p = Target("foo", "not a target type")
 
     def test_name_property(self):
         """Ensure that Target.name provides the correct value."""
-        p = Target("King Lear")
+        p = Target("King Lear", LIBRARY_TARGET)
         self.assertEqual(p.name, "King Lear")
+
+    def test_target_type_property(self):
+        """Ensure that Target.target_type provides the correct value."""
+        p = Target("King Lear", LIBRARY_TARGET)
+        self.assertEqual(p.target_type, LIBRARY_TARGET)
 
 
 if __name__ == "__main__":
